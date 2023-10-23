a1339676b699289e97fd2431b7e0d959213eb978
Ignorance <81954613+milkyicedtea@users.noreply.github.com>
2023-10-23 09:55:24 +0200

    I DID ALL THE CLASSES AND IT STILL WORKS

--- IgnoPlayer.py
+++ IgnoPlayer.py
@@ -1,129 +1,66 @@
-import os
-
-import pygame
-
 import tkinter as tk
-from tkinter import filedialog
 
 from MusicPlayer import MusicPlayer
 
-# Initialize Pygame
-pygame.mixer.init()
 
-# Initialize the music queue, current index, and folder path
-music_queue = []
-current_index = 0
-folder_path = ""
-paused = False
+class Window:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.dark_mode = True
+        self.player = MusicPlayer(self.window)
+        self.Buttons = {
+            "play_button": tk.Button(self.window, text = '▶ Play', command = self.player.play),
+            "stop_button": tk.Button(self.window, text = '⏹ Stop', command = self.player.stop),
+            "pause_button": tk.Button(self.window, text = '⏸ Pause', command = self.player.pause),
+            "prev_button": tk.Button(self.window, text = '⏮ Previous', command = self.player.previous_track),
+            "next_button": tk.Button(self.window, text = '⏭ Next', command = self.player.next_track),
+            "select_folder_button": tk.Button(self.window, text = 'Select Folder', command = self.player.select_folder),
+            "dark_mode_button": tk.Button(self.window, text = "Dark Mode", command = self.toggle_dark_mode, bg = "#34495E", fg = "white")
+        }
 
-# Create an empty list for storing file names
-file_names = []
+    def toggle_dark_mode(self):
+        self.dark_mode = not self.dark_mode
+        self.update_theme()
 
-# Declare listbox as a global variable
-listbox = None
+    def update_theme(self):
+        if self.dark_mode:
+            # Apply dark theme
+            self.window.configure(bg = "#121212")
+            self.Buttons['play_button'].configure(bg = "#2ECC71", fg = "white")
+            self.Buttons['stop_button'].configure(bg = "#F24D31", fg = "white")
+            self.Buttons['pause_button'].configure(bg = "#FFA500", fg = "white")
+        else:
+            # Apply light theme
+            self.window.configure(bg="#f0f0f0")
+            self.Buttons['play_button'].configure(bg="#008CBA", fg="white")
+            self.Buttons['stop_button'].configure(bg="#F24D31", fg="white")
+            self.Buttons['pause_button'].configure(bg="#FFA500", fg="white")
 
-# Declare window as a global variable
-window = None
+    def create_gui(self):
+        self.window.title("IgnoPlayer")
 
+        # Customize the appearance with some styling
+        self.window.geometry("1000x700")  # Set window size
+        self.window.configure(bg = "#f0f0f0")  # Set background color
 
-def play():
-    global music_queue, current_index, paused
-    if not music_queue:
-        return
+        # label = tk.Label(player.window, text="IgnoPlayer", bg="#f0f0f0", font=("Arial", 16))
+        # label.pack(pady=10)
 
-    if paused:
-        pygame.mixer.music.unpause()
-        paused = False
-    else:
-        pygame.mixer.music.load(music_queue[current_index])
-        pygame.mixer.music.play()
+        self.Buttons['play_button'].pack(side = tk.LEFT, padx = 10)
+        self.Buttons['stop_button'].pack(side = tk.LEFT, padx = 10)
+        self.Buttons['pause_button'].pack(side = tk.LEFT, padx = 10)
+        self.Buttons['prev_button'].pack(side = tk.LEFT, padx = 10)
+        self.Buttons['next_button'].pack(side = tk.LEFT, padx = 10)
+        self.Buttons['select_folder_button'].pack(side = tk.LEFT, padx = 10)
+        self.Buttons['dark_mode_button'].pack(side=tk.LEFT, padx=10)
 
+        self.update_theme()
 
-def stop():
-    global paused
-    pygame.mixer.music.stop()
-    paused = False
-
-
-def pause():
-    global paused
-    pygame.mixer.music.pause()
-    paused = True
-
-
-def next_track():
-    global current_index, paused
-    if music_queue:
-        current_index = (current_index + 1) % len(music_queue)
-        play()
-    else:
-        paused = False
-
-
-def previous_track():
-    global current_index, paused
-    if music_queue:
-        current_index = (current_index - 1) % len(music_queue)
-        play()
-    else:
-        paused = False
-
-
-def select_folder():
-    global folder_path, music_queue, file_names, listbox
-    folder_path = filedialog.askdirectory()
-    if folder_path:
-        selected_files = [f for f in os.listdir(folder_path) if f.lower().endswith(('.mp3', '.wav', '.flac'))]
-        selected_files = [os.path.join(folder_path, file) for file in selected_files]
-
-        if selected_files:
-            music_queue = selected_files
-            file_names = [os.path.basename(file) for file in selected_files]
-
-            if listbox:
-                listbox.destroy()  # Destroy the old Listbox if it exists
-
-            listbox = tk.Listbox(window, selectbackground = "#008CBA", font = ("Arial", 12))
-            listbox.pack(fill = tk.BOTH, expand = True, padx = 20, pady = 10)
-
-            for name in file_names:
-                listbox.insert(tk.END, name)
-
-            # print("Selected Files:", selected_files)  # Debugging output
-
-
-def create_gui():
-    global listbox, window  # Declare listbox and window as global variables
-
-    window = tk.Tk()
-    window.title("IgnoPlayer")
-
-    # Customize the appearance with some styling
-    window.geometry("1000x700")  # Set window size
-    window.configure(bg="#f0f0f0")  # Set background color
-
-    label = tk.Label(window, text="IgnoPlayer", bg="#f0f0f0", font=("Arial", 16))
-    label.pack(pady=10)
-
-    play_button = tk.Button(window, text = "▶ Play", command = play)
-    stop_button = tk.Button(window, text = "⏹ Stop", command = stop)
-    pause_button = tk.Button(window, text = "⏸ Pause", command = pause)
-    next_button = tk.Button(window, text = "⏭ Next", command = next_track)
-    prev_button = tk.Button(window, text = "⏮ Previous", command = previous_track)
-    select_folder_button = tk.Button(window, text = "Select Folder", command = select_folder)
-
-    play_button.pack(side = tk.LEFT, padx = 10)
-    stop_button.pack(side = tk.LEFT, padx = 10)
-    pause_button.pack(side = tk.LEFT, padx = 10)
-    next_button.pack(side = tk.LEFT, padx = 10)
-    prev_button.pack(side = tk.LEFT, padx = 10)
-    select_folder_button.pack(side = tk.LEFT, padx = 10)
-
-    listbox = tk.Listbox(window, selectbackground="#008CBA", font=("Arial", 12))
-    listbox.pack(fill=tk.BOTH, expand=True, padx=20, pady=10)
-
-    window.mainloop()
+        self.window.mainloop()
 
 
 if __name__ == "__main__":
-    create_gui()
+    app_window = Window()
+
+    # Create gui to open player and player window
+    app_window.create_gui()
--- MusicPlayer.py
+++ MusicPlayer.py
@@ -1,6 +1,74 @@
+import os
+
+import pygame
+import tkinter as tk
+from tkinter import filedialog
+
+
 class MusicPlayer:
-    def __init__(self):
-        ...
+    def __init__(self, window):
+        self.window = window
+        self.queue = []
+        self.current_index = 0
+        self.folder_path = ""
+        self.paused = False
+        self.mixer = pygame.mixer
+        self.listbox = None
+
+        if not self.mixer.get_init():
+            self.mixer.init()
+
+    def play(self):
+        if not self.queue:
+            return
+
+        if self.paused:
+            self.mixer.music.unpause()
+            self.paused = False
+        else:
+            self.mixer.music.load(self.queue[self.current_index])
+            self.mixer.music.play()
+
+    def stop(self):
+        self.mixer.music.stop()
+        self.paused = False
+
+    def pause(self):
+        pygame.mixer.music.pause()
+        self.paused = True
+
+    def next_track(self):
+        if self.queue:
+            self.current_index = (self.current_index + 1) % len(self.queue)
+            self.play()
+        else:
+            self.paused = False
+
+    def previous_track(self):
+        if self.queue:
+            self.current_index = (self.current_index - 1) % len(self.queue)
+            self.play()
+        else:
+            self.paused = False
+
+    def select_folder(self):
+        self.folder_path = filedialog.askdirectory()
+        if self.folder_path:
+            selected_files = [f for f in os.listdir(self.folder_path) if f.lower().endswith(('.mp3', '.wav', '.flac'))]
+            selected_files = [os.path.join(self.folder_path, file) for file in selected_files]
+
+            if selected_files:
+                self.queue = selected_files
+                file_names = [os.path.basename(file) for file in selected_files]
+
+                if self.listbox:
+                    self.listbox.destroy()  # Destroy the old Listbox if it exists
+
+                listbox = tk.Listbox(self.window, selectbackground = "#008CBA", font = ("Arial", 12))
+                listbox.pack(fill = tk.BOTH, expand = True, padx = 20, pady = 10)
+
+                for name in file_names:
+                    listbox.insert(tk.END, name)
 
 
 if __name__ == "__main__":
